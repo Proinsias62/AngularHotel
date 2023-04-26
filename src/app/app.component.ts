642be62e155e754bb113c29b1c25a0e39cee9ae7
Proinsias62 <33152994+Proinsias62@users.noreply.github.com>
2023-04-26 21:47:20 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hinv-root',
   templateUrl: './app.component.html',
+  //template: `<h1>Hello Word from Inline Template</h1>
+  //<p>Angular is awesome</p>`,
   styleUrls: ['./app.component.scss']
+  //styles: ['h1 {color: blue}']
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
+
+  role = "Admin";
 }
---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { Room, RoomList } from './rooms';
+
+@Component({
+  selector: 'hinv-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.scss']
+})
+
+export class RoomsComponent {
+
+  hotelName = "Kerry Gardens";
+  numberofRooms = "25";
+
+  hideRooms = true;
+
+  toggle() {
+    console.log("toggle()");
+    this.hideRooms = !this.hideRooms;
+
+  }
+
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 20,
+    bookedRooms: 5
+  }
+
+  roomList: RoomList[] = [
+    {
+      roomNumber: 1,
+      roomType: 'Deluxe',
+      amenities: 'air conditioning, TV, Wi-Fi',
+      price: 500,
+      photos: 'https://www.istockphoto.com/photo/modern-luxury-bedroom-gm1390233984-447267667',
+      checkInTime: new Date('04-21-2023'),
+      checkOutTime: new Date('04-22-2023'),
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Family',
+      amenities: 'air conditioning, TV, Wi-Fi',
+      price: 600,
+      photos: 'https://www.istockphoto.com/photo/modern-luxury-bedroom-gm1390233984-447267667',
+      checkInTime: new Date('04-21-2023'),
+      checkOutTime: new Date('04-22-2023'),
+    },
+    {
+      roomNumber: 3,
+      roomType: 'Standard',
+      amenities: 'TV, Wi-Fi',
+      price: 400,
+      photos: 'https://www.istockphoto.com/photo/modern-luxury-bedroom-gm1390233984-447267667',
+      checkInTime: new Date('04-21-2023'),
+      checkOutTime: new Date('04-22-2023'),
+    },
+  ]
+
+}
